a68435f31fbfea41799a9cfe9c49266972eb490e
cppc-PC\cppc <myeongseob91@naver.com>
2018-01-27 12:11:06 +0900

    start

---
+++ src/main/java/com/jasty/JastyApplication.java
@@ -0,0 +1,12 @@
+package com.jasty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JastyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JastyApplication.class, args);
+	}
+}
